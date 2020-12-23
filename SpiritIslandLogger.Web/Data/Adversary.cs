0ce3cf3539e8958d2ccc7b0e5692a5d203859215
Jupotter <jupotter@jupotter.eu>
2020-12-23 16:27:42 +0100

    Add adversay pages

--- SpiritIslandLogger.Web/Data/Adversary.cs
+++ SpiritIslandLogger.Web/Data/Adversary.cs
@@ -1,16 +1,26 @@
-﻿namespace SpiritIslandLogger.Web.Data
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace SpiritIslandLogger.Web.Data
 {
     public class Adversary
     {
-        public int        Id         { get; set; }
-        public string?    Name       { get; set; }
+        public int    Id   { get; set; }
         
-        public int Level0Difficulty { get; set; }
-        public int Level1Difficulty { get; set; }
-        public int Level2Difficulty { get; set; }
-        public int Level3Difficulty { get; set; }
-        public int Level4Difficulty { get; set; }
-        public int Level5Difficulty { get; set; }
-        public int Level6Difficulty { get; set; }
+        public string Name { get; set; } = null!;
+
+        [NotMapped]
+        public int[] Difficulty { get; set; } = new int[7];
+
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public string MappedDifficulty
+        {
+            get => string.Join(';', Difficulty);
+            set => Difficulty = value.Split(';', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
     }
 }
---
+++ SpiritIslandLogger.Web/Data/Migrations/20201223150848_Update adversary difficulty handling.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpiritIslandLogger.Web.Data.Migrations
+{
+    public partial class Updateadversarydifficultyhandling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Level0Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level1Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level2Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level3Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level4Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level5Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.DropColumn(
+                name: "Level6Difficulty",
+                table: "Adversaries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Adversaries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MappedDifficulty",
+                table: "Adversaries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MappedDifficulty",
+                table: "Adversaries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Adversaries",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level0Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level1Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level2Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level3Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level4Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level5Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level6Difficulty",
+                table: "Adversaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
