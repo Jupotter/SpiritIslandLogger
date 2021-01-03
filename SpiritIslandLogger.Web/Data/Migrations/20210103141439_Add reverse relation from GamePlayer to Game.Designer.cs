db924743ba8114c351a63c70579118456f2f8704
Jupotter <jupotter@jupotter.eu>
2021-01-03 15:19:30 +0100

    Add delete page for games

--- SpiritIslandLogger.Web/Data/Migrations/20210103141439_Add reverse relation from GamePlayer to Game.Designer.cs
+++ SpiritIslandLogger.Web/Data/Migrations/20210103141439_Add reverse relation from GamePlayer to Game.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpiritIslandLogger.Web.Data;
 
 namespace SpiritIslandLogger.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210103141439_Add reverse relation from GamePlayer to Game")]
+    partial class AddreverserelationfromGamePlayertoGame
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SpiritIslandLogger.Web/Data/Migrations/20210103141439_Add reverse relation from GamePlayer to Game.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpiritIslandLogger.Web.Data.Migrations
+{
+    public partial class AddreverserelationfromGamePlayertoGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GamePlayers_Games_GameId",
+                table: "GamePlayers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameId",
+                table: "GamePlayers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GamePlayers_Games_GameId",
+                table: "GamePlayers",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GamePlayers_Games_GameId",
+                table: "GamePlayers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GameId",
+                table: "GamePlayers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GamePlayers_Games_GameId",
+                table: "GamePlayers",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
