d946ba58f1f3f1a6f6ff94cb69780cd2f3b2f4f4
Jupotter <jupotter@jupotter.eu>
2021-02-12 15:45:30 +0100

    Store score in database

--- SpiritIslandLogger.Web/Data/Game.cs
+++ SpiritIslandLogger.Web/Data/Game.cs
@@ -23,46 +23,8 @@ namespace SpiritIslandLogger.Web.Data
         public int?           FearLevel        { get; set; }
         public int?           InvaderCardsLeft { get; set; }
 
+        public int? Score { get; set; }
+
         public string Comment { get; set; } = "";
-
-        [NotMapped]
-        public int? RealScore
-        {
-            get
-            {
-                if (ManualScore.HasValue)
-                    return ManualScore;
-                if (Players?.Count > 0      &&
-                    DahanLeft.HasValue      &&
-                    BlightCount.HasValue    &&
-                    BlightedIsland.HasValue &&
-                    InvaderCardsLeft.HasValue)
-                {
-                    var score = DahanLeft / Players.Count - BlightCount / Players.Count;
-
-                    int difficulty = 0;
-                    int deckSize   = 12;
-                    if (Adversary != null && AdversaryLevel.HasValue && Adversary.Levels != null)
-                    {
-                        var level = Adversary.Levels.First(l => l.Level == AdversaryLevel.Value);
-                        difficulty = level.Difficulty;
-                        deckSize   = level.DeckSize;
-                    }
-
-                    if (Victory)
-                    {
-                        score += 5 * difficulty + 10 + 2 * InvaderCardsLeft;
-                    }
-                    else
-                    {
-                        score += 2 * difficulty + 2 * (deckSize - InvaderCardsLeft);
-                    }
-
-                    return score;
-                }
-
-                return null;
-            }
-        }
     }
 }
---
+++ SpiritIslandLogger.Web/Data/Migrations/20210212143354_Store score in the game table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpiritIslandLogger.Web.Data.Migrations
+{
+    public partial class Storescoreinthegametable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Games",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Games");
+        }
+    }
+}
